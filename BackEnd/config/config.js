3f84bce581b4312d2e10ba839fbcac15b940fbd3
Emanuel <182751243+Cloud-exe@users.noreply.github.com>
2024-10-06 20:13:51 +0000

    config db

--- BackEnd/config/config.js
+++ BackEnd/config/config.js
@@ -1,33 +1,37 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: 'postgres', 
+// });
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'postgres', 
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, // Si necesitas SSL
+            rejectUnauthorized: false // Cambia a true si quieres verificar el certificado
+        }
+    }
 });
 
+// Probar la conexión
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida con éxito.');
+    })
+    .catch(err => {
+        console.error('No se pudo conectar a la base de datos:', err);
+    });
+
 module.exports = sequelize;
-
-
-
-// const { Sequelize } = require('sequelize');
-// require('dotenv').config();
-
-// const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// });
-
-// module.exports = sequelize;
--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
 const CONFIG = {
-    API_URL: 'http://localhost:3000',
+    API_URL: 'https://ubiquitous-disco-jjq457p45r49fx4v-3000.app.github.dev',
 };
+
+// const CONFIG = {
+//     API_URL: 'localhost:3000',
+// };
