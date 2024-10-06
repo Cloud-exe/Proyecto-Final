d877061ed54867d6a9948cf44d665cf6b588cd16
Emanuel <182751243+Cloud-exe@users.noreply.github.com>
2024-10-06 13:06:29 +0000

    setupserver

--- BackEnd/config/config.js
+++ BackEnd/config/config.js
@@ -1,10 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres', // Asegúrate de que el dialecto esté configurado
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 module.exports = sequelize;
